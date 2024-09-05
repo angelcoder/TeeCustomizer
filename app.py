d4ac1c57210b7be6c1c6de2b409b8db361cc603d
Lina Logvina <31827866+angelcoder@users.noreply.github.com>
2024-09-05 17:08:44 +0300

    user issue button and handling added

--- app.py
+++ app.py
@@ -2,17 +2,36 @@ import chainlit as cl
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
 from src.llm import ask_order, messages
+from src.user_issue import UserIssue
 
 load_dotenv()
 client = AsyncOpenAI()
 cl.instrument_openai()
 
+user_issue = UserIssue()
+
 @cl.on_message
 async def on_message(message: cl.Message):
     messages.append({"role": "user", "content": message.content})
-    response = ask_order(messages)
-    messages.append({"role": "assistant", "content": response})
 
-    await cl.Message(
-        content=response,
-    ).send()
+    global user_issue
+    response, user_issue = ask_order(messages)
+
+    if response:
+        messages.append({"role": "assistant", "content": response})
+
+    if response:
+        await cl.Message(
+            content=response,
+        ).send()
+    else:
+        actions = [
+            cl.Action(name="Chat support team", value="Chat support team", description="Click me!")
+        ]
+
+        await cl.Message(content="Press the button and we will redirect you to the support team", actions=actions).send()
+
+@cl.action_callback("Chat support team")
+async def on_action(action):
+    await cl.Message(content=f"Hi! This is {action.name}\nIssue: {user_issue.issue_type}\nDetails: {user_issue.details}").send()
+    await action.remove()
--- src/llm.py
+++ src/llm.py
@@ -1,5 +1,7 @@
+import json
 from openai import OpenAI
 from src.prompt import system_instruction
+from src.user_issue import UserIssue, functions, handle_user_issue
 
 client = OpenAI()
 
@@ -12,7 +14,26 @@ def ask_order(messages, model="gpt-4", temperature=0):
     response = client.chat.completions.create(
         model=model,
         messages=messages,
-        temperature=temperature
+        temperature=temperature,
+        functions=functions,
+        function_call="auto"
     )
 
-    return response.choices[0].message.content
+    user_issue = UserIssue()
+    # Check the response from the API
+    if response.choices[0].message.function_call:
+        function_name = response.choices[0].message.function_call.name
+        function_arguments = response.choices[0].message.function_call.arguments
+
+        if function_name == "handle_user_issue":
+            function_arguments = json.loads(function_arguments)
+
+            issue_type = function_arguments.get("issue_type", None)
+            details = function_arguments.get("details", None)
+
+            user_issue.set_issue_type(issue_type)
+            user_issue.set_details(details)
+
+            handle_user_issue(user_issue)
+
+    return response.choices[0].message.content, user_issue
